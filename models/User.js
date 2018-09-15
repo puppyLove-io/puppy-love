b8800c63807bbef907b6f9efb57903d3baf3df34
Jayce Azua <jayce.azua@gmail.com>
2018-09-15 16:36:08 -0700

    setting up userSchema

--- models/User.js
+++ models/User.js
@@ -2,13 +2,23 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name : String,
-  business : String,
+  username: String,
+  firstName : String,
+  lastName: String,
+  location: {
+    address: String,
+    state: String,
+    country: String,
+    zipcode: Number,
+    lat: String,
+    long: String
+  },
   googleId : String,
   accessToken : String,
   refreshToken : String,
   phone : String,
   email : String,
+
   admin : { type: Boolean, default: false }
 });
 
