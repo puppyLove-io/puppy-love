d31e7eaff87842e7f868694b45b24de4fc8cf4a7
Jayce Azua <jayce.azua@gmail.com>
2018-09-15 17:54:30 -0700

    google auth

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 //prod.js - production keys
 module.exports = {
-  googleClientID: 'PUT YOURS HERE',
-  googleClientSecret: 'PUT YOURS HERE',
+  googleClientID: '945603290675-umsebqdjod82nkq7ndhe4mn6hi88f8g2.apps.googleusercontent.com',
+  googleClientSecret: 'sqVPptqxHd0m3NOqJr1PdcME',
   cookieKey: 'cookie',
 }
