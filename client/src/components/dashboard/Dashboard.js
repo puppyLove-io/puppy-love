33694414336aebb66a17d12b2323ed02c0863f55
Joerez <rezenbiz@gmail.com>
2018-09-17 13:15:26 -0700

    better landing page

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 
 class Dashboard extends Component {
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <h2 align="center">Log in pls</h2>
+      default:
+        return (
+          <h2 align="center">Hi {this.props.auth.username}</h2>
+        )
+    }
+  }
+
+
   render() {
+
     return (
-      <div className="dashboard">Dashboard</div>
+      <div className="dashboard">{this.renderContent()}</div>
     )
   }
 }
 
-export default Dashboard
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps, actions)(Dashboard);
--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Landing extends Component {
   render() {
     return (
-      <div className="dashboard">Landing Page</div>
+      <div className="dashboard">
+      <h2 align="center">Puppy love helps you breed your doggos.</h2>
+      <h3 align="center"><a href="/auth/google">Try for free</a></h3>
+      </div>
     )
   }
 }
